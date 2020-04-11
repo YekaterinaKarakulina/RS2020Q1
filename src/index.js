8b506994cc4e02d072da718848ea039cff70aad3
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2020-04-11 15:26:43 +0400

    feat: add styles for trainCard

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import Cards from './js/Cards';
 import cardsActionA from './js/cardsData/cardsActionA';
 import cardsMainPage from './js/cardsData/cardsMainPage';
 
-const cardsWrapper = document.querySelector('.cardsWrapper');
+//const cardsWrapper = document.querySelector('.cardsWrapper');
 
 //const cards = (new Cards('categoryCards')).generateSectionCards(cardsActionA);
-const cards = (new Cards('sectionCards')).generateSectionCards(cardsMainPage)
-cardsWrapper.append(cards);
+//const cards = (new Cards('sectionCards')).generateSectionCards(cardsMainPage)
+//cardsWrapper.append(cards);
 
   
 /*
