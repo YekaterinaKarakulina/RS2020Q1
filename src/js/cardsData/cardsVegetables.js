695864ca661b7822651c812d17e9b840ff061d71
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2020-04-17 22:19:36 +0400

    feat: add new data for 2 sections: vegetables and weather

---
+++ src/js/cardsData/cardsVegetables.js
@@ -0,0 +1,53 @@
+const cardsVegetables = [
+    {
+      word: 'cabbage',
+      translation: 'капуста',
+      imageSrc: './src/assets/images/cardsVegetables/cabbage.jpg',
+      audioSrc: './src/assets/sounds/cardsVegetables/cabbage.mp3',
+    },
+    {
+      word: 'carrot',
+      translation: 'морковь',
+      imageSrc: './src/assets/images/cardsVegetables/carrot.jpg',
+      audioSrc: './src/assets/sounds/cardsVegetables/carrot.mp3',
+    },
+    {
+      word: 'garlic',
+      translation: 'чеснок',
+      imageSrc: './src/assets/images/cardsVegetables/garlic.jpg',
+      audioSrc: './src/assets/sounds/cardsVegetables/garlic.mp3',
+    },
+    {
+      word: 'onion',
+      translation: 'лук',
+      imageSrc: './src/assets/images/cardsVegetables/onion.jpg',
+      audioSrc: './src/assets/sounds/cardsVegetables/onion.mp3',
+    },
+    {
+      word: 'pepper',
+      translation: 'перец',
+      imageSrc: './src/assets/images/cardsVegetables/pepper.jpg',
+      audioSrc: './src/assets/sounds/cardsVegetables/pepper.mp3',
+    },
+    {
+      word: 'potato',
+      translation: 'картофель',
+      imageSrc: './src/assets/images/cardsVegetables/potato.jpg',
+      audioSrc: './src/assets/sounds/cardsVegetables/potato.mp3',
+    },
+    {
+      word: 'pumpkin',
+      translation: 'тыква',
+      imageSrc: './src/assets/images/cardsVegetables/pumpkin.jpg',
+      audioSrc: './src/assets/sounds/cardsVegetables/pumpkin.mp3',
+    },
+    {
+      word: 'tomato',
+      translation: 'помидор',
+      imageSrc: './src/assets/images/cardsVegetables/tomato.jpg',
+      audioSrc: './src/assets/sounds/cardsVegetables/tomato.mp3',
+    },
+  ];
+  
+  export default cardsVegetables;
+  
