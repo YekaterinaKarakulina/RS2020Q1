008c1ee08783e236e9214458ba02922a383d2a08
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2020-05-04 18:03:53 +0400

    feat: implement printSearchResults, clearInputValue methods

--- movie-search/src/index.js
+++ movie-search/src/index.js
@@ -8,11 +8,34 @@ require('@babel/polyfill');
 
 const apiKey = '5964eff4';
 
+const searchResultsTitle = document.querySelector('.search__resultsTitle');
+const searchResultsMessage = document.querySelector('.search__resultsMessage');
+
+function setFocus() {
+  document.querySelector('.search__inputField').focus();
+}
+
+function printSearchResults(movieForSearch) {
+  if (document.querySelector('.search__inputField').getAttribute('bla') === 'true') {
+    searchResultsTitle.textContent = 'Search results for: ';
+    searchResultsMessage.textContent = movieForSearch;
+  } else {
+    searchResultsTitle.textContent = 'No results for: ';
+    searchResultsMessage.textContent = movieForSearch;
+  }
+}
+
 async function translate(word) {
   const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200322T155651Z.de98a60e6a99185e.089aea4237b51c6db082c966f27a7895cd1e8b44&text=${word}&lang=ru-en`;
   const res = await fetch(url);
   const data = await res.json();
   return data.text[0];
+}
+
+function clearInputValue() {
+  document.querySelector('.search__inputField').value = '';
+  searchResultsTitle.textContent = '';
+  searchResultsMessage.textContent = '';
 }
 
 async function readInputValue() {
@@ -35,15 +58,18 @@ async function getMovieImdbRating(imdbID) {
 async function getMovieInfo(title) {
   const url = `https://www.omdbapi.com/?s=${title}&apikey=${apiKey}`;
   const res = await fetch(url);
-  console.log(res);
   const data = await res.json();
-  console.log(`data.totalResults: ${data.totalResults}`);
   console.log(data);
-  console.log(data.Search);
-  return data.Search;
+  if (data.Response === 'False') {
+    console.log(data.Error);
+    document.querySelector('.search__inputField').setAttribute('bla', 'false');
+  } else {
+    document.querySelector('.search__inputField').setAttribute('bla', 'true');
+    return data.Search;
+  }
 }
 
-async function getMovieObject(data, i) {
+async function createMovieObject(data, i) {
   const movieObject = {
     title: data[i].Title,
     year: data[i].Year,
@@ -62,32 +88,45 @@ function createMovieCard(movieObject) {
   return cardItem;
 }
 
-async function renderRequestResults(movieForSearch) {
-  const data = await getMovieInfo(movieForSearch);
-  // console.log(data.length);
-  for (let i = 0; i < data.length; i += 1) {
-    const movieObject = await getMovieObject(data, i);
-    swiper.appendSlide(createMovieCard(movieObject));
+async function renderRequestResults(data) {
+  const slides = [];
+  if (data) {
+    for (let i = 0; i < data.length; i += 1) {
+      const movieObject = await createMovieObject(data, i);
+      slides.push(createMovieCard(movieObject));
+    }
   }
+  slides.forEach((slide) => {
+    swiper.appendSlide(slide);
+  });
+}
+
+async function firstRequest(movieForSearch) {
+  document.querySelector('.search__inputField').setAttribute('bla', 'true');
+  printSearchResults(movieForSearch);
+  const data = await getMovieInfo(movieForSearch);
+  await renderRequestResults(data);
 }
 
 async function searchButtonHandler() {
   const movieForSearch = await readInputValue();
-  swiper.removeAllSlides();
-  await renderRequestResults(movieForSearch);
+  const data = await getMovieInfo(movieForSearch);
+  printSearchResults(movieForSearch);
+  if (document.querySelector('.search__inputField').getAttribute('bla') === 'true') {
+    swiper.removeAllSlides();
+    renderRequestResults(data);
+  }
 }
 
-async function firstRequest(movieForSearch) {
-  await renderRequestResults(movieForSearch);
-}
-
-function setFocus() {
-  document.querySelector('.search__inputField').focus();
-}
-
+const firstRequestValue = 'Home alone';
 setFocus();
-firstRequest('home alone');
+firstRequest(firstRequestValue);
 
 document.querySelector('.search__button').addEventListener('click', () => {
   searchButtonHandler();
+});
+
+document.querySelector('.icon__delete').addEventListener('click', () => {
+  console.log('del');
+  clearInputValue();
 });
