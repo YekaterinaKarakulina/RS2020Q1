27750a4b5b134e31c60b375b71ad7c5e6d095630
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2020-05-05 18:07:07 +0400

    feat: add new slodes loading

--- movie-search/src/index.js
+++ movie-search/src/index.js
@@ -13,6 +13,11 @@ const searchResultsMessage = document.querySelector('.search__resultsMessage');
 const searchInputField = document.querySelector('.search__inputField');
 const searchErrorMessage = document.querySelector('.search__errorMessage');
 
+let sMovieForSearch = 'Home alone';
+let iPageNumber = 1;
+let bFlag = false;
+
+
 function setFocus() {
   searchInputField.focus();
 }
@@ -64,9 +69,9 @@ async function getMovieImdbRating(imdbID) {
   return data;
 }
 
-async function getMovieInfo(title) {
+async function getMovieInfo(title, page) {
   searchInputField.setAttribute('isResponseOk', 'false');
-  const url = `https://www.omdbapi.com/?s=${title}&apikey=${apiKey}`;
+  const url = `https://www.omdbapi.com/?s=${title}&page=${page}&apikey=${apiKey}`;
   const res = await fetch(url);
   const data = await res.json();
   console.log(data);
@@ -99,6 +104,27 @@ function createMovieCard(movieObject) {
   return cardItem;
 }
 
+async function needToLoad() {
+  const array = document.querySelector('.swiper-wrapper').children;
+  console.log(array.length);
+  const el = array[array.length - 7];
+  if (el.classList.contains('swiper-slide-active')) {
+    console.log('need To load');
+    iPageNumber += 1;
+    console.log(sMovieForSearch, iPageNumber);
+    if (sMovieForSearch) {
+      try {
+        const data = await getMovieInfo(sMovieForSearch, iPageNumber);
+        if (searchInputField.getAttribute('isResponseOk') === 'true') {
+          renderRequestResults(data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+}
+
 async function renderRequestResults(data) {
   const slides = [];
   if (data) {
@@ -111,19 +137,20 @@ async function renderRequestResults(data) {
   slides.forEach((slide) => {
     swiper.appendSlide(slide);
   });
+  await needToLoad(data);
 }
 
 async function firstRequest(movieForSearch) {
   searchInputField.setAttribute('isResponseOk', 'true');
-  const data = await getMovieInfo(movieForSearch);
+  const data = await getMovieInfo(movieForSearch, iPageNumber);
   await renderRequestResults(data);
 }
 
 async function searchButtonHandler() {
-  const movieForSearch = await readInputValue();
-  if (movieForSearch) {
+  sMovieForSearch = await readInputValue();
+  if (sMovieForSearch) {
     try {
-      const data = await getMovieInfo(movieForSearch);
+      const data = await getMovieInfo(sMovieForSearch, iPageNumber);
       if (searchInputField.getAttribute('isResponseOk') === 'true') {
         document.querySelector('.spinner').classList.remove('hidden');//
         swiper.removeAllSlides();
@@ -135,9 +162,8 @@ async function searchButtonHandler() {
   }
 }
 
-const firstRequestValue = 'Home alone';
 setFocus();
-firstRequest(firstRequestValue);
+firstRequest(sMovieForSearch);
 
 document.querySelector('.search__button').addEventListener('click', () => {
   searchButtonHandler();
@@ -145,4 +171,14 @@ document.querySelector('.search__button').addEventListener('click', () => {
 
 document.querySelector('.icon__delete').addEventListener('click', () => {
   clearInputValue();
+});
+
+document.querySelector('.swiper-button-next').addEventListener('click', () => {
+  needToLoad();
+});
+
+document.querySelector('.swiper-pagination').addEventListener('click', (event) => {
+  if (event.target.classList.contains('swiper-pagination-bullet')) {
+    needToLoad();
+  }
 });
