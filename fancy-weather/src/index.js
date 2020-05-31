810abf3bac686054b483859c7ba6f91c84c27383
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2020-05-31 10:38:59 +0400

    voice api move to separate modul

--- fancy-weather/src/index.js
+++ fancy-weather/src/index.js
@@ -7,7 +7,9 @@ import getUserGeolocation from './js/APIs/userGeolocationAPI';
 import { getAndRenderNewImage } from './js/render/renderImage';
 import { languages } from './js/data/data';
 import { switchLanguage } from './js/utils/translationsUtils';
-import getLanguageAbbreviation from './js/utils/localStorageUtils';
+// import getLanguageAbbreviation from './js/utils/localStorageUtils';
+// import { recognitionRestart, tellWeather } from './js/APIs/voiceAPI';
+import voiceRecognitionAndSpeak from './js/APIs/voiceAPI';
 
 const cityInput = document.querySelector('.input__city');
 const submitButton = document.querySelector('.submit__button');
@@ -84,7 +86,6 @@ async function searchFromInputForm(city) {
 
 
 submitButton.addEventListener('click', (event) => {
-  
   cityInput.focus();
   event.preventDefault();
   const city = cityInput.value;
@@ -110,163 +111,4 @@ document.querySelector('.dropdown-menu').addEventListener('click', (event) => {
   switchLanguage(localStorage.getItem('language'), appObject);
 });
 
-
-const microphone = document.querySelector('.icon__microphone');
-
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new SpeechRecognition();
-recognition.continuous = true;
-
-const msg = new SpeechSynthesisUtterance();
-msg.volume = 0.5;
-
-function recognitionRestart() {
-  recognition.stop();
-  setTimeout(() => {
-    recognition.start();
-  }, 400);
-  recognition.continuous = true;
-}
-
-function tellWeather() {
-  speechSynthesis.cancel();
-  const date = document.querySelector('.date').innerText.split(' ');
-
-  const monthDay = date[1] + date[2];
-  const currentTemperature = document.querySelector('.currentTemperature').innerText;
-
-  const weatherDescription = document.querySelector('.weatherDescription').innerText;
-  const realFeelTempTitle = document.querySelector('.realFeelTempTitle').innerText;
-  const realFeelTempValue = document.querySelector('.realFeelTempValue').innerText;
-
-  const windSpeedTitle = document.querySelector('.windSpeedTitle').innerText;
-  const windSpeedValue = document.querySelector('.windSpeedValue').innerText.split(' ')[0];
-
-  const humidityTitle = document.querySelector('.humidityTitle').innerText;
-  const humidityValue = document.querySelector('.humidityValue').innerText;
-  const language = getLanguageAbbreviation(localStorage.getItem('language'));
-  msg.rate = 0.8;
-  msg.pitch = 1;
-
-  let template = '';
-
-  switch (language) {
-    case 'ru':
-      msg.lang = 'ru';
-      template = `Сегодня ${monthDay}. Температура воздуха ${currentTemperature} °. ${weatherDescription}.
-      ${realFeelTempTitle} ${realFeelTempValue} °. ${windSpeedTitle} ${windSpeedValue} метров в секунду.
-      ${humidityTitle} ${humidityValue} %.`;
-      break;
-    case 'be':
-      msg.lang = 'ru';
-      template = `сёння ${monthDay}. Тэмпература паветра ${currentTemperature} °. ${weatherDescription}.
-      ${realFeelTempTitle} ${realFeelTempValue} °. ${windSpeedTitle} ${windSpeedValue} метраў у секунду.
-      ${humidityTitle} ${humidityValue} %.`;
-      break;
-    default:
-      msg.lang = 'en';
-      template = `Today is ${monthDay}. Current temperature ${currentTemperature} °. ${weatherDescription}.
-      ${realFeelTempTitle} ${realFeelTempValue} °. ${windSpeedTitle} ${windSpeedValue} meters per second.
-      ${humidityTitle} ${humidityValue} %.`;
-      break;
-  }
-  console.log(template);
-  console.log(msg.lang);
-  console.log(msg.volume);
-  msg.text = template;
-  speechSynthesis.speak(msg);
-}
-
-
-microphone.addEventListener('click', () => {
-  microphone.classList.toggle('active');
-  if (microphone.classList.contains('active')) {
-    recognition.lang = getLanguageAbbreviation(localStorage.getItem('language'));
-    recognition.start();
-  } else {
-    recognition.stop();
-  }
-});
-
-
-recognition.addEventListener('result', (event) => {
-  const transcript = Array.from(event.results)
-    .map((result) => result[0])
-    .map((result) => result.transcript);
-  console.log(transcript);
-  const language = getLanguageAbbreviation(localStorage.getItem('language'));
-  switch (language) {
-    case 'en':
-      switch (transcript[0]) {
-        case 'weather':
-          recognitionRestart();
-          tellWeather();
-          break;
-        case 'quieter':
-          recognitionRestart();
-          msg.volume -= 0.2;
-          break;
-        case 'louder':
-          recognitionRestart();
-          msg.volume += 0.2;
-          break;
-        default:
-          cityInput.value = transcript;
-          submitButton.click();
-          recognition.stop();
-          microphone.classList.remove('active');
-          break;
-      }
-      break;
-    case 'ru':
-      switch (transcript[0]) {
-        case 'погода':
-          recognitionRestart();
-          tellWeather();
-          break;
-        case 'тише':
-          recognitionRestart();
-          msg.volume -= 0.2;
-          break;
-        case 'громче':
-          recognitionRestart();
-          msg.volume += 0.2;
-          break;
-        default:
-          cityInput.value = transcript;
-          submitButton.click();
-          recognition.stop();
-          microphone.classList.remove('active');
-          break;
-      }
-      break;
-    case 'be':
-      switch (transcript[0]) {
-        case 'надвор\'е':
-          recognitionRestart();
-          tellWeather();
-          break;
-        case 'цішэй':
-          recognitionRestart();
-          msg.volume -= 0.2;
-          break;
-        case 'гучней':
-          recognitionRestart();
-          msg.volume += 0.2;
-          break;
-        default:
-          cityInput.value = transcript;
-          submitButton.click();
-          recognition.stop();
-          microphone.classList.remove('active');
-          break;
-      }
-      break;
-    default:
-      break;
-  }
-});
-
-document.querySelector('.icon__sound').addEventListener('click', () => {
-  tellWeather();
-});
+voiceRecognitionAndSpeak();
